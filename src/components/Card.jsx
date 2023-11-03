13473d20bbc369ca757c9585f3750625758e1e55
jaar88 <almandorios@gmail.com>
2023-11-03 07:16:01 -0300

    Carrito de compras

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import '../styles/card.css'
+
+
+export const Card = ({imagen, titulo, descripcion, precio, handleAgregar, handleQuitar, handleAumentar, handleDiminuir}) => {
+
+    const [added, setAdded] = useState(false)
+
+    const clickAgregar = () =>{
+        handleAgregar()
+        setAdded(true)
+    }
+
+    const clickQuitar = () =>{
+        handleQuitar()
+        setAdded(false)
+    }
+    
+
+    return (
+    <div className="tarjeta">
+        <img src={imagen} alt="titulo" className="tarjeta-imagen"></img>
+        <div className="tarjeta-contenido">
+            <h3 className="tarjeta-titulo">{titulo}</h3>
+            <p className="tarjeta-descripcion">{descripcion}</p>
+            <p className="tarjeta-precio">{precio}</p>
+        
+        {added
+            ? <button 
+            type="button" 
+            className="boton-quitar"
+            onClick={clickQuitar}
+            >Quitar del carrito </button>
+            : <button 
+            type="button" 
+            className="boton-agregar"
+            onClick={clickAgregar}
+            >Agregar al carrito </button>
+        }
+        </div>
+    </div>
+  )
+}
